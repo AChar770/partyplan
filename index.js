c3ac43ec6f0f38a81d94eef80e60f399d2985aef
AChar770 <amandatcharles@yahoo.com>
2026-01-14 22:35:17 -0500

    updated getParties, and getParty

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let guests = [];
 /** Updates state with all parties from the API */
 async function getParties() {
   try {
-    const response = await fetch(API + "/events");
+    const response = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2511-FTB-CT-WEB-PT/events");
     const result = await response.json();
     parties = result.data;
     render();
@@ -24,7 +24,7 @@ async function getParties() {
 /** Updates state with a single party from the API */
 async function getParty(id) {
   try {
-    const response = await fetch(API + "/events/" + id);
+    const response = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2511-FTB-CT-WEB-PT/events/{id}");
     const result = await response.json();
     selectedParty = result.data;
     render();
